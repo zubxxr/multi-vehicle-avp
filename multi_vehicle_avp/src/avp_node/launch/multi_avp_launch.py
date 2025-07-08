5643b5b8c531ea5649c3b30495641f98637fb279
Zubair Islam <91695367+zubxxr@users.noreply.github.com>
2025-07-07 22:12:57 -0400

    fix(multi_avp_launch): add delay to each node launch to avoid cpu overload

--- multi_vehicle_avp/src/avp_node/launch/multi_avp_launch.py
+++ multi_vehicle_avp/src/avp_node/launch/multi_avp_launch.py
@@ -1,6 +1,6 @@
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, TimerAction
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch.conditions import IfCondition
 from launch_ros.actions import Node
@@ -19,60 +19,89 @@ def generate_launch_description():
 
     return LaunchDescription([
 
-        DeclareLaunchArgument('avp_file', default_value='avp_node', description='Which AVP executable to run'),
+        DeclareLaunchArgument('avp_file', default_value='avp_state_machine_node', description='Which AVP executable to run'),
+        # DeclareLaunchArgument('avp_file', default_value='avp_node', description='Which AVP executable to run'),
         DeclareLaunchArgument('vehicle_id', default_value='1', description='Vehicle ID to pass to the AVP script'),
         DeclareLaunchArgument('enable_managers', default_value='true', description='Enable manager nodes'),
         DeclareLaunchArgument('debug', default_value='false', description='Enable debug mode (simulator use)'),
         DeclareLaunchArgument('namespaces', default_value='[main, vehicle2]', description='List of namespaces'),
 
-        ExecuteProcess(
-            cmd=['bash', script_path],
-            output='screen',
+        TimerAction(
+            period=0.5,
+            actions=[
+                ExecuteProcess(
+                    cmd=['bash', script_path],
+                    output='screen',
+                )
+            ]
         ),
 
-        # Manager Nodes (conditionally launched)
-        Node(
-            package='avp_managers',
-            executable='drop_off_zone_queue_manager',
-            name='drop_off_zone_queue_manager',
-            output='screen',
-            arguments=['--ros-args', '-p', ['namespaces:=', LaunchConfiguration('namespaces')]],
-            condition=IfCondition(enable_managers)
+        TimerAction(
+            period=1.0,
+            actions=[
+                Node(
+                    package='avp_managers',
+                    executable='drop_off_zone_queue_manager',
+                    name='drop_off_zone_queue_manager',
+                    output='screen',
+                    arguments=['--ros-args', '-p', ['namespaces:=', LaunchConfiguration('namespaces')]],
+                    condition=IfCondition(enable_managers)
+                )
+            ]
         ),
-        Node(
-            package='avp_managers',
-            executable='parking_spot_reservation_manager',
-            name='parking_spot_reservation_manager',
-            output='screen',
-            arguments=['--ros-args', '-p', ['namespaces:=', LaunchConfiguration('namespaces')]],
-            condition=IfCondition(enable_managers)
+        TimerAction(
+            period=2.5,
+            actions=[
+                Node(
+                    package='avp_managers',
+                    executable='parking_spot_reservation_manager',
+                    name='parking_spot_reservation_manager',
+                    output='screen',
+                    arguments=['--ros-args', '-p', ['namespaces:=', LaunchConfiguration('namespaces')]],
+                    condition=IfCondition(enable_managers)
+                )
+            ]
         ),
-        Node(
-            package='avp_managers',
-            executable='vehicle_count_manager',
-            name='vehicle_count_manager',
-            output='screen',
-            arguments=['--ros-args', '-p', ['namespaces:=', LaunchConfiguration('namespaces')]],
-            condition=IfCondition(enable_managers)
+        TimerAction(
+            period=4.0,
+            actions=[
+                Node(
+                    package='avp_managers',
+                    executable='vehicle_count_manager',
+                    name='vehicle_count_manager',
+                    output='screen',
+                    arguments=['--ros-args', '-p', ['namespaces:=', LaunchConfiguration('namespaces')]],
+                    condition=IfCondition(enable_managers)
+                )
+            ]
         ),
-
-        Node(
-            package='avp_managers',
-            executable='vehicle_status_manager',
-            name='vehicle_status_manager',
-            output='screen',
-            arguments=['--ros-args', '-p', ['namespaces:=', LaunchConfiguration('namespaces')]],
-            condition=IfCondition(enable_managers)
+        TimerAction(
+            period=5.0,
+            actions=[
+                Node(
+                    package='avp_managers',
+                    executable='vehicle_status_manager',
+                    name='vehicle_status_manager',
+                    output='screen',
+                    arguments=['--ros-args', '-p', ['namespaces:=', LaunchConfiguration('namespaces')]],
+                    condition=IfCondition(enable_managers)
+                )
+            ]
         ),
 
         # AVP Node
-        Node(
-            package='avp_node',
-            executable=avp_file,
-            output='screen',
-            arguments=[
-                '--vehicle_id', vehicle_id,
-                '--debug', LaunchConfiguration('debug')
-            ],
+        TimerAction(
+            period=5.5,
+            actions=[
+                Node(
+                    package='avp_node',
+                    executable=avp_file,
+                    output='screen',
+                    arguments=[
+                        '--vehicle_id', vehicle_id,
+                        '--debug', LaunchConfiguration('debug')
+                    ],
+                )
+            ]
         ),
     ])
